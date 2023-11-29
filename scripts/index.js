270f3563b1235ba0b516123c102979cc8dd5367c
greatoni <greatoni3@gmail.com>
2023-11-28 21:58:51 -0500

    modal, cards, and some CSS

--- scripts/index.js
+++ scripts/index.js
@@ -7,21 +7,38 @@ const category3 = document.getElementById("category3")
 
 const displayDiv = document.getElementById("display");
 
+const cart = [];
+
 const baseUrl = "https://fakestoreapi.com/"
+
+//Variables for CartModal items;
+const cartB = document.getElementById('cartButton');
+
+const tableB = document.getElementById('tableBody');
+
+const subtotalP = document.getElementById('Subtotal');
+const taxP = document.getElementById('Tax');
+const shippingP = document.getElementById('Shipping');
+const totalP = document.getElementById('Total');
+
+const clearB = document.getElementById('clearCart');
+const purchaseB = document.getElementById('purchase')
 
 // async function fake store
 async function fakeStore(endpoint)
 {
     let fakePromise = await fetch(`${baseUrl}${endpoint}`);
 
-    console.log(fakePromise);
+    let jsonPromise = await fakePromise.json();
 
-    displayCards('');
+    //console.log(jsonPromise);
+
+    displayCards(jsonPromise);
 }
 
 onload = () => {
     fakeStore("products");
-    console.log(category1)
+    //console.log(category1)
 }
 
 //* Event Listeners
@@ -29,22 +46,41 @@ onload = () => {
 category1.addEventListener('click', e => {
     e.preventDefault();
 
-    console.log('selected game section')
-    fakeStore("products/category/game");
+    //console.log('selected game section')
+    fakeStore("products/category/electronics");
 })
 
 category2.addEventListener('click', e => {
     e.preventDefault();
 
-    console.log('selected book section')
-    fakeStore("products/category/book");
+    //console.log('selected book section')
+    fakeStore("products/category/jewelery");
 })
 
 category3.addEventListener('click', e => {
     e.preventDefault();
 
-    console.log('selected mini section')
-    fakeStore("products/category/mini");
+    //console.log('selected mini section')
+    fakeStore("products/category/men's clothing");
+})
+
+cartB.addEventListener('click', e => {
+    e.preventDefault();
+
+    //console.log('selected mini section')
+    displayCart();
+})
+
+clearB.addEventListener('click', e => {
+    // Purchase and clear do the same things, purchase just does something more!
+    e.preventDefault();
+    emptyCart();
+})
+
+purchaseB.addEventListener('click', e => {
+    e.preventDefault();
+    emptyCart();
+    //! AN ALERT SHOULD BE PLACED HERE!!!!!!!
 })
 
 //*Functions:
@@ -56,10 +92,14 @@ function emptyElements(element) {
     }
 }
 
-function displayCards(parameter)
+function displayCards(endpoint)
 {
+    console.log(endpoint)
 
     emptyElements(displayDiv)
+
+    for (items in endpoint)
+    {
 
     // A loop should be created starting here that loops through each product within a category,
     //*create elements
@@ -71,8 +111,15 @@ function displayCards(parameter)
 
     let body = document.createElement('div')
 
+    //Append both the description and price to this div INSTEAD of the body.
+    let accordion = document.createElement('div');
+
     // These elements are listed in the order they are appended
     let descAccordion = document.createElement('div');
+    let descAccHeader = document.createElement('h2');
+    let descAccordionButton = document.createElement('button')
+    let descCollapse = document.createElement('div');
+    let descBody = document.createElement('div')
     let desc = document.createElement('p')
     // List End
 
@@ -90,21 +137,56 @@ function displayCards(parameter)
     newCard.className = 'card';
 
     img.className = 'cardImg';
+    img.src = endpoint[items].image
 
     body.className = 'cardBody';
 
-    title.textContent = "Placeholder!"
+    title.textContent = endpoint[items].title;
     title.className = 'title';
 
-    descAccordion.className = 'accordionClass'
-    desc.textContent = "PlaceHolder!"
+    accordion.className = 'accordion';
+    accordion.id = `cardAccordion${endpoint[items].id}`
+
+    descAccordion.className = 'accordion-item'
+    descAccHeader.className = 'accordion-header'
+    
+    descAccordionButton.className = 'accordion-button collapsed'
+    descAccordionButton.textContent = 'Description'
+    descAccordionButton.setAttribute("data-bs-toggle","collapse")
+    descAccordionButton.setAttribute('data-bs-target',`#descCollapse${endpoint[items].id}`)
+    descAccordionButton.setAttribute('aria-expanded', 'true')
+    descAccordionButton.setAttribute('aria-controls', 'collapseOne');
+
+    descCollapse.className = "accordionCollapse collapse"
+    descCollapse.id = `descCollapse${endpoint[items].id}`
+    descCollapse.setAttribute('data-bs-parent',`cardAccordion${endpoint[items].id}`)
+    
+    descBody.className = 'accordion-body'
+
+    desc.textContent = endpoint[items].description
     desc.className = 'desc'
 
-    priceAccordion.className = 'accordionClass'
-    price.textContent = "0$"
+    priceAccordion.className = 'accordion'
+    price.textContent = `${endpoint[items].price.toFixed(2)}$`
     price.className = 'price'
 
     add.className = 'addbutton'
+
+    let id = endpoint[items].id;
+    let itemTitle = endpoint[items].title;
+    let itemcost = endpoint[items].price
+    
+    add.onclick = () =>
+    {
+        console.log(items)
+        let abbridgedItem = {
+            id : id,
+            title : itemTitle,
+            cost : itemcost,
+            quantity : 1,
+        }
+        submitToCart(abbridgedItem)
+    }
     add.textContent = 'Add to Cart!'
 
     //append to parent
@@ -114,17 +196,115 @@ function displayCards(parameter)
     priceAccordion.appendChild(price);
 
     body.appendChild(title)
-    body.appendChild(descAccordion)
-    body.appendChild(priceAccordion)
+    
+    descBody.appendChild(desc);
+    descCollapse.appendChild(descBody);
+    
+    descAccHeader.appendChild(descAccordionButton);
+    
+    descAccordion.appendChild(descAccHeader);
+    descAccordion.appendChild(descCollapse);
+    accordion.appendChild(descAccordion)
+
+
+
+    accordion.appendChild(priceAccordion)
+
+    body.appendChild(accordion);
     body.appendChild(add)
 
     newCard.appendChild(img)
     newCard.appendChild(body)    
 
     displayDiv.appendChild(newCard)
+    }
 }
 
-function addToCart()
+function submitToCart(item)
 {
-    console.log("This function has not been implemented yet.")
+    let ispresent = false;
+    for(items in cart)
+    {
+
+        if(cart[items].id === item.id) {
+        cart[items].quantity += item.quantity;
+        ispresent = true;
+        }
+    }
+
+    if(!ispresent)
+    {
+        cart.push(item);
+    }
+    console.log(cart);
+}
+
+//Functions regarding the cart are placed here!
+function emptyCart() {
+    while(cart.length > 0)
+    {
+        cart.pop();
+    }
+    displayCart();
+}
+
+function displayCart() {
+    //empty elements
+    emptyElements(tableB);
+
+
+    //create and populate table rows with cart items,
+    for(items in cart)
+    {
+        //create
+        let newRow = document.createElement('tr')
+        let quantityHeader = document.createElement('th')
+        let itemT = document.createElement('td')
+        let priceT = document.createElement('td')
+
+        //assign
+
+        quantityHeader.setAttribute('scope','row');
+
+        quantityHeader.textContent = cart[items].quantity;
+        itemT.textContent = `${cart[items].title} for ${cart[items].cost} each.`
+        let multiprice = (cart[items].cost * cart[items].quantity)
+        priceT.textContent = `${multiprice.toFixed(2)}$`;
+
+        //append
+        newRow.appendChild(quantityHeader);
+        newRow.appendChild(itemT);
+        newRow.appendChild(priceT);
+
+        tableB.appendChild(newRow);
+
+    }
+
+    //determine the totals of various items!
+    accessTotals();
+}
+
+
+// access the various totals of the numbers.
+function accessTotals() {
+    let subTotal = 0;
+    let taxRate = 0.07;
+    let shippingCost = 0.10;
+
+    for (items in cart)
+    {
+        subTotal += (cart[items].cost * cart[items].quantity)
+        console.log(subTotal);
+    }
+    let taxes = (subTotal * taxRate)
+    let taxedTotal = (subTotal + taxes);
+    let shippingCosts = (taxedTotal * shippingCost)
+    let totalTotal = (taxedTotal + shippingCosts);
+
+    subtotalP.textContent = `${subTotal.toFixed(2)}$`;
+    taxP.textContent = `${taxes.toFixed(2)}$`;
+    shippingP.textContent = `${shippingCosts.toFixed(2)}$`;
+    totalP.textContent = `${totalTotal.toFixed(2)}$`;
+
+    purchaseB.textContent = `Purchase for ${totalTotal.toFixed(2)}`
 }
